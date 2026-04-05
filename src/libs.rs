d1a55c4916c7c3d73ff8cc775e58a5132a0bf113
pavelbaryshnikov <baryshnikovpol@gmail.com>
2026-04-05 09:39:11 +0200

    add simple refill logic and tests

--- src/libs.rs
+++ src/libs.rs
@@ -1,4 +1,5 @@
 use std::time::Instant;
+use std::cmp;
 
 pub struct TokenBucket {
     capacity: u64,
@@ -22,6 +23,12 @@ impl TokenBucket {
     }
 
     fn try_consume_at(&mut self, tokens: u64, now: Instant) -> bool {
+        let time_from_last_refill = (now - self.last_refill).as_secs();
+        if time_from_last_refill > 0 {
+            self.tokens = cmp::min(self.capacity, self.tokens + self.refill_rate * time_from_last_refill);
+            self.last_refill = now;
+        }
+
         if self.tokens >= tokens {
             self.tokens -= tokens;
             true
@@ -43,5 +50,30 @@ mod tests {
         assert!(bucket.try_consume(1));
         assert!(bucket.try_consume(1));
         assert!(!bucket.try_consume(1));
+    }
+
+    #[test]
+    fn refills_after_time_passed() {
+        let start = Instant::now();
+        let mut bucket = TokenBucket::new(3, 1);
+        assert!(bucket.try_consume_at(3, start));
+        assert!(!bucket.try_consume_at(1, start));
+
+        let one_second_later = start + std::time::Duration::from_secs(1);
+        assert!(bucket.try_consume_at(1, one_second_later));
+    }
+
+    #[test]
+    fn refills_only_after_time_passed() {
+        let mut bucket = TokenBucket::new(3, 1);
+        let start = bucket.last_refill;
+        assert!(bucket.try_consume_at(3, start));
+
+
+        let half_second_later = start + std::time::Duration::from_millis(500);
+        assert!(!bucket.try_consume_at(1, half_second_later));
+
+        let one_second_later = half_second_later + std::time::Duration::from_millis(500);
+        assert!(bucket.try_consume_at(1, one_second_later));
     }
 }
