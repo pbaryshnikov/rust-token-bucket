e5d1195e6a67826bfeb97a582f5aa6617a194742
pavelbaryshnikov <baryshnikovpol@gmail.com>
2026-04-05 08:56:26 +0200

    initial commit

---
+++ src/libs.rs
@@ -0,0 +1,13 @@
+pub struct TokenBucket {
+
+}
+
+impl TokenBucket {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn try_consume(&mut self, tokens: u64) -> bool {
+        todo!()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod libs;
+
+fn main() {
+    println!("Hello, world!");
+}
